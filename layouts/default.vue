9b8d14d1c7c285d8581fa74e0515cfc7db862d9b
suzunedp <i0ho8ku0ij1nja_1029@me.com>
2020-03-27 15:06:29 +0900

    ビデオのモーダルを追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,8 +18,9 @@ export default {
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
+  font-size: 14px;
   word-spacing: 1px;
+  line-height: 21px;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
--- pages/_project/index.vue
+++ pages/_project/index.vue
@@ -5,7 +5,15 @@
         h1.detail__title {{jsondata[$route.params.project].title}}
         p.detail__msg {{jsondata[$route.params.project].msg_en}}
         p.detail__msg {{jsondata[$route.params.project].msg_jp}}
+        
+        //- video on PC and tablet
+        .detail__videobutton.link-button#link-button(v-on:click = "vbuttonClicked") VIDEO
 
+        .detail__modal-wrapper#modal-wrapper(v-on:click = "modalClicked" v-if="active")
+            youtube(:video-id="videoId")
+                //- iframe(width="100%" height="100%" :src="jsondata[$route.params.project].video" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
+        //- video on mobile
         .detail__video
             iframe(width="100%" height="100%" :src="jsondata[$route.params.project].video" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
@@ -16,24 +24,55 @@
             img.detail__pic(v-for="(url, index) in jsondata[$route.params.project].img" :key="index" :src="url")
                 //- img(:src="url")
 
-        a.detail__link(v-for="(link, index) in jsondata[$route.params.project].link" :key="index" :href="link" target="_blank" rel="noopener noreferrer") {{ link }} <font-awesome-icon icon="external-link-alt"/>
+        a.detail__link.link-button-wh(v-for="(link, index) in jsondata[$route.params.project].link" :key="index" :href="link" target="_blank" rel="noopener noreferrer") VISIT WEBSITE
 
         nuxt-link.detail__back-button.link-button(to="/") ←BACK
 
 </template>
 <script>
+//     // IFrame Player API の読み込み
+//     var tag = document.createElement('script');
+//     tag.src = "https://www.youtube.com/iframe_api";
+//     var firstScriptTag = document.getElementsByTagName('script')[0];
+//     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+//     function onYouTubeIframeAPIReady() {
+//         ytPlayer = new YT.Player(
+//             'modal-video', // 埋め込む場所の指定
+//             {
+//                 width: 640, // プレーヤーの幅
+//                 height: 390, // プレーヤーの高さ
+//                 videoId: 'cB7Il-HaLpY' // YouTubeのID
+//             }
+//         );
+//     }
+// };
+
 import jsonfile from '~/assets/projects.json';
 
 export default {
     data(){
         return{
             jsondata: jsonfile,
+            active: false,
+            videoId: 'cB7Il-HaLpY'
         }
     },
     mounted(){
         this.$store.commit("updatePage","detail")
-    }
+    },
+
+    methods:{
+        vbuttonClicked: function(){
+            this.active = true;
+        },
+        modalClicked: function(){
+            this.active = false;
+        }
+
+    } 
 }
+
 </script>
 
 <style lang="stylus" scoped>
@@ -52,17 +91,39 @@ export default {
         //     text-align center
         &__title, &__msg, &__video
             text-align center
-        &__video
-            position relative
-            width 100%
-            margin 24px 0
-            padding calc( 100vw * 0.8 * 9 / 16 ) 0 0
-            iframe
-                position absolute
-                top 0
+        &__videobutton
+            display block
+            text-align center
+
+        &__modal
+            &-wrapper
+                display flex
+                position fixed
+                z-index 1
                 left 0
-                width 100%
-                height 100%
+                top 0
+                height 100vh
+                width 100vw
+                overflow auto
+                background-color rgba(0 0 0 0.7)
+                align-items center
+                justify-content center
+
+            &-video
+                display block
+                position relative
+                width 70vw
+                margin 24px 0
+                padding calc( 100vw * 0.7 * 9 / 16 ) 0 0
+                iframe
+                    position absolute
+                    top 0
+                    left 0
+                    width 100%
+                    height 100%
+        
+        &__video
+            display none
         &__desc
             padding 16px 0 
         &__picwrapper
@@ -73,10 +134,8 @@ export default {
             height auto
             object-fit cover
         &__link
-            border 1px solid txt-color
-            padding 16px
-            margin 8px 0
-            text-decoration none
+            text-align center
+            border 3px solid txt-color
             display block
         &__back-button
             text-align center
@@ -96,6 +155,16 @@ export default {
             &__link
                 margin 0
             &__video
+                display block
+                position relative
+                width 100%
+                margin 24px 0
                 padding calc( (100vw - 32px) * 9 / 16 ) 0 0
+                iframe
+                    position absolute
+                    top 0
+                    left 0
+                    width 100%
+                    height 100%
 
 </style>
